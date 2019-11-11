f8220798b6e43c9aa4a0de1b446e155389aba656
Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
2019-11-11 14:56:57 +0100

    virtio: [DRAFT] add a vhost driver for SOF DSPs
    This adds support for audio DSPs, running SOF. See
    https://www.sofproject.org/ for details of the project.
    
    Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

--- include/standard-headers/linux/virtio_ids.h
+++ include/standard-headers/linux/virtio_ids.h
@@ -44,5 +44,6 @@
 #define VIRTIO_ID_VSOCK        19 /* virtio vsock transport */
 #define VIRTIO_ID_CRYPTO       20 /* virtio crypto */
 #define VIRTIO_ID_PMEM         27 /* virtio pmem */
+#define VIRTIO_ID_DSP          28 /* virtio DSP */
 
 #endif /* _LINUX_VIRTIO_IDS_H */
